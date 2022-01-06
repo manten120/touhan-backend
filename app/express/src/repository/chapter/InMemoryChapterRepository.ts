cb483c5fdfa987fbfc1610cd956f3014aca52e43
manten120 <hamaw8888@gmail.com>
2022-01-06 23:21:17 +0900

    chapterのインメモリリポジトリを作成

---
+++ app/express/src/repository/chapter/InMemoryChapterRepository.ts
@@ -0,0 +1,47 @@
+import type { IChapterRepository } from '../../domain/models/chapter/IChapterRepository';
+import { Chapter } from '../../domain/models/chapter/Chapter';
+import { IChapterFactory } from '../../domain/models/chapter/IChapterFactory';
+import { ChapterNum } from '../../domain/models/chapter/ChapterNum';
+
+export class InMemoryChapterRepository implements IChapterRepository {
+  private readonly chapterFactory: IChapterFactory;
+
+  constructor(chapterFactory: IChapterFactory) {
+    this.chapterFactory = chapterFactory;
+  }
+
+  public store = new Map<string, Chapter>();
+
+  save = async (chapter: Chapter) => {
+    const clonedChapter = this.clone(chapter);
+    this.store.set(JSON.stringify(clonedChapter.num), clonedChapter);
+  };
+
+  find = async (chapterNum: ChapterNum) => {
+    const chapter = this.store.get(JSON.stringify(chapterNum));
+
+    if (!chapter) {
+      return null;
+    }
+    return this.clone(chapter);
+  };
+
+  findAll = async () => {
+    const result: Chapter[] = [];
+
+    this.store.forEach((value) => {
+      result.push(this.clone(value));
+    });
+
+    return result;
+  };
+
+  delete = async (chapter: Chapter) => {
+    this.store.delete(JSON.stringify(chapter.num));
+  };
+
+  private readonly clone = (chapter: Chapter) => {
+    const clonedChapter = this.chapterFactory.create(chapter.num.value, chapter.title.value);
+    return clonedChapter;
+  };
+}
