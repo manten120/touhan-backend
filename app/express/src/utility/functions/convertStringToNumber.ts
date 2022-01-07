d9a1a044ef4003d33b173cdda01c0138ffc621d0
manten120 <hamaw8888@gmail.com>
2022-01-07 23:11:27 +0900

    空文字列やスペース、真偽値の場合にも対応

--- app/express/src/utility/functions/convertStringToNumber.ts
+++ app/express/src/utility/functions/convertStringToNumber.ts
@@ -8,12 +8,17 @@ export const convertStringToNumber = (str: string) => {
   // strに含まれる全角数字を半角数字にする
   const replacedStr = str.replace(/[０-９]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 0xfee0));
 
-  // 数値にする。replacedStrが数字以外の文字を含む場合NaNになる
-  const num = Number(replacedStr);
+  // 数値にする。replacedStrが数字以外の文字を含む場合NaNになる。
+  // Number()の注意点: 空文字列、スペース、falseは0、trueは1になる
+  const num1 = Number(replacedStr);
 
-  if (Number.isNaN(num)) {
+  // 数値にする。replacedStrが数字以外の文字を含む場合NaNになる。
+  // parseInt()の注意点: 真偽値、空文字列、スペースはNaNになる。文字列に数字以外の文字を含む場合、数字以外の文字の直前までの数字を数値に変換する。数字以外の文字で始まる場合はNaN
+  const num2 = parseInt(replacedStr, 10);
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
     throw new Error('引数strに数字でない文字を含むため半角数値への変換に失敗しました');
   }
 
-  return num;
+  return num1;
 };
